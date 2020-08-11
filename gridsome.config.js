a7970b514ed89ccde85bad9e6ca2e3e93ba3768d
Lucien Bénié <lucien.benie@logmein.com>
2020-08-11 14:33:24 -0400

    add basic stuff

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,33 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: [],
+  titleTemplate: '%s',
+  siteUrl: 'http://localhost:8080', // change me
+  plugins: [
+    {
+      use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: '',
+      },
+    },
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        cacheTime: 600000,
+      },
+    },
+    {
+      use: 'gridsome-plugin-robots-txt',
+    },
+    {
+      use: '@gridsome/source-contentful',
+      options: {
+        space: process.env.CTF_SPACE_ID,
+        accessToken: process.env.CTF_ACCESS_TOKEN,
+        host: 'cdn.contentful.com',
+        environment: 'master',
+        typeName: 'Contentful',
+      },
+    },
+  ],
 }
